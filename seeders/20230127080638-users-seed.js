6ae1a652af0c8f81f73e33fe162c54efca2a4994
HowardWu5566 <ggg1237890@gmail.com>
2023-01-28 14:06:15 +0800

    feat: modify seed-users

--- seeders/20230127080638-users-seed.js
+++ seeders/20230127080638-users-seed.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const bcrypt = require('bcryptjs')
-const faker = require('faker')
-
+const { TEACHERS_AMOUNT, STUDENTS_AMOUNT } = require('./seeders-amount')
 /** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     const DEFAULT_PASSWORD = 'password'
@@ -17,12 +17,23 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
-      ...Array.from({ length: 10 }, (_, index) => ({
-        name: `seed-user${index + 1}`,
-        email: `user${index + 1}@example.com`,
+      ...Array.from({ length: TEACHERS_AMOUNT }, (_, index) => ({
+        name: `seed-teacher${index + 1}`,
+        email: `teacher${index + 1}@example.com`,
         password: bcrypt.hashSync(DEFAULT_PASSWORD, bcrypt.genSaltSync(10)),
-        role: 'user',
+        role: 'teacher',
         avatar: `https://loremflickr.com/320/240/man,woman/?lock=${index + 1}`,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      ...Array.from({ length: STUDENTS_AMOUNT }, (_, index) => ({
+        name: `seed-student${index + 1}`,
+        email: `student${index + 1}@example.com`,
+        password: bcrypt.hashSync(DEFAULT_PASSWORD, bcrypt.genSaltSync(10)),
+        role: 'student',
+        avatar: `https://loremflickr.com/320/240/man,woman/?lock=${
+          index + TEACHERS_AMOUNT + 1
+        }`,
         createdAt: new Date(),
         updatedAt: new Date()
       }))
@@ -34,8 +45,12 @@ module.exports = {
       name: [
         'root',
         ...Array.from(
-          { length: SEED_USERS_AMOUNT },
-          (_, i) => `seed-user${i + 1}`
+          { length: TEACHERS_AMOUNT },
+          (_, index) => `seed-teacher${index + 1}`
+        ),
+        ...Array.from(
+          { length: STUDENTS_AMOUNT },
+          (_, index) => `seed-student${index + 1}`
         )
       ]
     })
