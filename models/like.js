a21efe6d5adcf0c145834afce91dfae3cc81840a
HowardWu5566 <ggg1237890@gmail.com>
2023-01-31 12:05:21 +0800

    feat: add seed-likes

--- models/like.js
+++ models/like.js
@@ -3,11 +3,6 @@
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Like extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Like.belongsTo(models.User, { foreignKey: 'userId' })
       if (this.object === 'question') {
@@ -15,15 +10,14 @@ module.exports = (sequelize, DataTypes) => {
       } else if (this.object === 'reply') {
         Like.belongsTo(Reply, { foreignKey: 'object_id' })
       }
-
-      // define association here
     }
   }
   Like.init(
     {
       userId: DataTypes.INTEGER,
       object: DataTypes.STRING,
-      objectId: DataTypes.INTEGER
+      objectId: DataTypes.INTEGER,
+      isSeed: DataTypes.BOOLEAN
     },
     {
       sequelize,
---
+++ seeders/20230127083553-likes-seed.js
@@ -0,0 +1,83 @@
+'use strict'
+const { User, Question, Reply } = require('../models')
+const {
+  LIKE_QUESTION_AMOUNT,
+  LIKE_REPLY_AMOUNT
+} = require('../helpers/seeders-amount')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const userIdArr = await User.findAll({
+      raw: true,
+      nest: true,
+      where: Sequelize.or(
+        { name: { [Sequelize.Op.like]: 'seed-student%' } },
+        { name: { [Sequelize.Op.like]: 'seed-teacher%' } }
+      ),
+      attributes: ['id']
+    })
+    const questionIdArr = await Question.findAll({
+      raw: true,
+      nest: true,
+      where: {
+        description: {
+          [Sequelize.Op.like]: 'seed-question%'
+        }
+      },
+      attributes: ['id']
+    })
+    const replyIdArr = await Reply.findAll({
+      raw: true,
+      nest: true,
+      where: {
+        comment: {
+          [Sequelize.Op.like]: 'seed-reply%'
+        }
+      },
+      attibutes: ['id']
+    })
+    const likes = []
+    do {
+      let userId
+      let questionId
+      do {
+        userId = userIdArr[Math.floor(Math.random() * userIdArr.length)].id
+        questionId =
+          questionIdArr[Math.floor(Math.random() * questionIdArr.length)].id
+      } while (likes.join('-').includes(`${userId},question,${questionId}`))
+      likes.push([userId, 'question', questionId])
+    } while (likes.length < LIKE_QUESTION_AMOUNT)
+
+    do {
+      let userId
+      let replyId
+      do {
+        userId = userIdArr[Math.floor(Math.random() * userIdArr.length)].id
+        replyId = replyIdArr[Math.floor(Math.random() * replyIdArr.length)].id
+      } while (likes.join('-').includes(`${userId},reply,${replyId}`))
+      likes.push([userId, 'reply', replyId])
+    } while (likes.length < LIKE_QUESTION_AMOUNT + LIKE_REPLY_AMOUNT)
+
+    await queryInterface.bulkInsert(
+      'Likes',
+      Array.from(
+        { length: LIKE_QUESTION_AMOUNT + LIKE_REPLY_AMOUNT },
+        (_, index) => ({
+          userId: likes[index][0],
+          object: likes[index][1],
+          objectId: likes[index][2],
+          isSeed: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      )
+    )
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Likes', {
+      isSeed: true
+    })
+  }
+}
