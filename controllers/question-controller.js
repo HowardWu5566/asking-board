98670a65bc52cb95f55b9d5718297521c8056a90
HowardWu5566 <ggg1237890@gmail.com>
2023-02-02 12:04:48 +0800

    feat: get question

--- controllers/question-controller.js
+++ controllers/question-controller.js
@@ -45,6 +45,55 @@ const questionController = {
     } catch (error) {
       next(error)
     }
+  },
+  getQuestion: async (req, res, next) => {
+    try {
+      const currentUserId = 12
+      const question = await Question.findByPk(req.params.id, {
+        raw: true,
+        nest: true,
+        attributes: [
+          'id',
+          'description',
+          'image',
+          'isAnonymous',
+          'grade',
+          'subject',
+          'createdAt',
+          [
+            sequelize.literal(
+              '(SELECT COUNT(id) FROM Replies WHERE Replies.questionId = Question.id)'
+            ),
+            'replyCount'
+          ],
+          [
+            sequelize.literal(
+              '(SELECT COUNT(id) FROM Likes WHERE Likes.object = "question" AND Likes.objectId = Question.id)'
+            ),
+            'likeCount'
+          ],
+          [
+            sequelize.literal(
+              `EXISTS (SELECT id FROM Likes WHERE Likes.userId = ${sequelize.escape(
+                currentUserId
+              )} AND Likes.object = "question" AND Likes.objectId = Question.id)`
+            ),
+            'isLiked'
+          ]
+        ],
+        include: {
+          model: User,
+          attributes: ['id', 'name', 'avatar']
+        }
+      })
+      if (!question)
+        return res
+          .status(404)
+          .json({ status: 404, message: "question doesn't exist!" })
+      return res.status(200).json(question)
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
--- routes/modules/question.js
+++ routes/modules/question.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const questionController = require('../../controllers/question-controller')
 
 router.get('/', questionController.getQuestions)
+router.get('/:id', questionController.getQuestion)
 
 module.exports = router
