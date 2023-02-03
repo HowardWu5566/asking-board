0977cb78f98f00e1414df29d79570acf8a4430aa
HowardWu5566 <ggg1237890@gmail.com>
2023-02-03 12:34:38 +0800

    feat: delete reply

--- controllers/reply-controller.js
+++ controllers/reply-controller.js
@@ -25,6 +25,25 @@ const replyController = {
     } catch (error) {
       next(error)
     }
+  },
+  deleteReply: async (req, res, next) => {
+    try {
+      const userId = 12
+      const replyId = req.params.id
+      const reply = await Reply.findByPk(replyId)
+      if (!reply)
+        return res
+          .status(404)
+          .json({ status: 404, message: "reply doesn't exist!" })
+      if (reply.userId !== userId)
+        return res
+          .status(401)
+          .json({ status: 'error', message: 'unauthorized!' })
+      await reply.destroy()
+      return res.status(200).json({ status: 'success' })
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
--- routes/modules/reply.js
+++ routes/modules/reply.js
@@ -2,5 +2,7 @@ const router = require('express').Router()
 const replyController = require('../../controllers/reply-controller')
 
 router.put('/:id', replyController.putReply)
+router.delete('/:id', replyController.deleteReply)
+
 
 module.exports = router
