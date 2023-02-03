859abc34b8cde9cee8f66a94f18c3f6b9d5b222c
HowardWu5566 <ggg1237890@gmail.com>
2023-02-03 13:29:06 +0800

    feat: add reply-related APIs -merge feat/reply

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const question = require('./modules/question')
+const reply = require('./modules/reply')
 
 router.use('/api/v1/questions', question)
+router.use('/api/v1/replies', reply)
 
 router.get('/', (req, res) => {
   res.send('routes')
