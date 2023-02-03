f91a01c4cf01e4f777244bedf60022163cd72540
HowardWu5566 <ggg1237890@gmail.com>
2023-02-03 12:52:33 +0800

    feat: post like to replies

--- controllers/reply-controller.js
+++ controllers/reply-controller.js
@@ -1,4 +1,4 @@
-const { Reply } = require('../models')
+const { Reply, Like } = require('../models')
 
 const replyController = {
   putReply: async (req, res, next) => {
@@ -41,6 +41,32 @@ const replyController = {
           .json({ status: 'error', message: 'unauthorized!' })
       await reply.destroy()
       return res.status(200).json({ status: 'success' })
+    } catch (error) {
+      next(error)
+    }
+  },
+  postReplyLike: async (req, res, next) => {
+    try {
+      const userId = 12
+      const replyId = req.params.id
+      const reply = await Reply.findByPk(replyId)
+      if (!reply)
+        return res
+          .status(404)
+          .json({ status: 'error', message: 'reply doesn,t exist!' })
+      const like = await Like.findOrCreate({
+        where: {
+          userId,
+          object: 'reply',
+          objectId: replyId
+        },
+        defaults: { isSeed: false }
+      })
+      if (!like[1])
+        return res
+          .status(422)
+          .json({ status: 'error', message: 'already like the reply!' })
+      return res.status(422).json({ status: 'success' })
     } catch (error) {
       next(error)
     }
