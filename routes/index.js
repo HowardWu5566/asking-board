dd9c18601c872c866814d0d24a98ae73be4ba78a
HowardWu5566 <ggg1237890@gmail.com>
2023-02-04 10:34:29 +0800

    feat: add follow-related APIs -merge feat/followship

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const question = require('./modules/question')
 const reply = require('./modules/reply')
+const followship = require('./modules/followship')
 
 router.use('/api/v1/questions', question)
 router.use('/api/v1/replies', reply)
+router.use('/api/v1/followships', followship)
 
 router.get('/', (req, res) => {
   res.send('routes')
