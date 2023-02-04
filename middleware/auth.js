5ca0703cc4386ac5e95424e3e13185fdf08098df
HowardWu5566 <ggg1237890@gmail.com>
2023-02-04 16:17:11 +0800

    feat: add user authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,5 +11,12 @@ module.exports = {
       req.user = user.dataValues
       next()
     })(req, res, next)
+  },
+  authenticatedUser: (req, res, next) => {
+    if (req.user.role === 'student' || req.user.role === 'teacher')
+      return next()
+    return res
+      .status(403)
+      .json({ status: 'error', message: 'Permission denied' })
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,15 @@ const reply = require('./modules/reply')
 const followship = require('./modules/followship')
 const userController = require('../controllers/user-controller')
 
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedUser } = require('../middleware/auth')
 const { undefinedRoute } = require('../middleware/error-handler')
 
 router.post('/api/v1/signup', userController.signUp)
 router.post('/api/v1/users/login', userController.login)
 
-router.use('/api/v1/questions', authenticated, question)
-router.use('/api/v1/replies', authenticated, reply)
-router.use('/api/v1/followships', authenticated, followship)
+router.use('/api/v1/questions', authenticated, authenticatedUser, question)
+router.use('/api/v1/replies', authenticated, authenticatedUser, reply)
+router.use('/api/v1/followships', authenticated, authenticatedUser, followship)
 
 router.use('*', undefinedRoute)
 
