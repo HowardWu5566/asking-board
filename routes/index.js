9e20d50c747b165dea4119932fc557a951f6b54e
HowardWu5566 <ggg1237890@gmail.com>
2023-02-04 11:58:59 +0800

    feat: add error handlers -merge feat/error-handler

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
 const question = require('./modules/question')
 const reply = require('./modules/reply')
 const followship = require('./modules/followship')
+const { undefinedRoute } = require('../middleware/error-handler')
 
 router.use('/api/v1/questions', question)
 router.use('/api/v1/replies', reply)
 router.use('/api/v1/followships', followship)
 
-router.get('/', (req, res) => {
-  res.send('routes')
-})
+router.use('*', undefinedRoute)
 
 module.exports = router
