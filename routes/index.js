d562c35378d802a638b6e017eae746e28a3a2dc5
HowardWu5566 <ggg1237890@gmail.com>
2023-02-04 13:10:06 +0800

    feat: add user-related APIs -merge feat/user

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const router = require('express').Router()
 const question = require('./modules/question')
 const reply = require('./modules/reply')
 const followship = require('./modules/followship')
+const userController = require('../controllers/user-controller')
+
 const { undefinedRoute } = require('../middleware/error-handler')
+
+router.post('/api/v1/signup', userController.signUp)
+router.post('/api/v1/users/login', userController.login)
 
 router.use('/api/v1/questions', question)
 router.use('/api/v1/replies', reply)
