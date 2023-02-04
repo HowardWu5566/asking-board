a8cbfe1591efa1222ea62f8caa2fb4b71bf7392c
HowardWu5566 <ggg1237890@gmail.com>
2023-02-04 11:54:35 +0800

    feat: add undefined path error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,12 @@
 module.exports = {
-  errorHandler: (error, res, next) => {
+  errorHandler: (error, res) => {
     return res
       .status(error.status || 500)
       .json({ status: 'error', message: 'unexpected error' })
+  },
+  undefinedRoute: (req, res) => {
+    return res
+      .status(404)
+      .json({ status: 'error', message: 'path not found' })
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
+const { undefinedRoute } = require('../middleware/error-handler')
 
-router.get('/', (req, res) => {
-  res.send('routes')
-})
+router.use('*', undefinedRoute)
 
 module.exports = router
