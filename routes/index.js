2d1fd6c2506cc344009afec0e1903c5a9ac6e27d
HowardWu5566 <ggg1237890@gmail.com>
2023-02-06 11:53:08 +0800

    feat: add admin-related APIs -merge feat/admin

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const reply = require('./modules/reply')
 const followship = require('./modules/followship')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
+const adminController = require('../')
 const { undefinedRoute } = require('../middleware/error-handler')
 const {
   authenticated,
@@ -13,11 +14,11 @@ const {
 
 router.post('/api/v1/signup', userController.signUp)
 router.post('/api/v1/users/login', userController.login)
+router.post('/api/v1/admin/login', adminController.login)
 
 router.use('/api/v1/questions', authenticated, authenticatedUser, question)
 router.use('/api/v1/replies', authenticated, authenticatedUser, reply)
 router.use('/api/v1/followships', authenticated, authenticatedUser, followship)
-
 router.use('/api/v1/admin', authenticated, authenticatedAdmin, admin)
 
 router.use('*', undefinedRoute)
---
+++ routes/modules/admin.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const adminController = require('../../controllers/admin-controller')
+
+router.get('/questions', adminController.getquestions)
+router.delete('/questions/:id', adminController.deleteQuestion)
+router.delete('/replies/:id', adminController.deleteReply)
+router.get('users', adminController.getUsers)
+
+module.exports = router
