d701cf618e41667d560453a6f47e346fdd371946
HowardWu5566 <ggg1237890@gmail.com>
2023-02-28 23:38:03 +0800

    fix: production config

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 
 const routes = require('./routes')
 const { errorHandler } = require('./middleware/error-handler')
 const passport = require('./config/passport')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -12,6 +15,6 @@ app.use(passport.initialize())
 app.use(routes)
 app.use(errorHandler)
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('app is running')
 })
