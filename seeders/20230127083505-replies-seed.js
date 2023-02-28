7ecc29d7bc758e66767530f58b4549bbe0989d26
HowardWu5566 <ggg1237890@gmail.com>
2023-02-28 22:04:04 +0800

    feat: modify seed data

--- seeders/20230127083505-replies-seed.js
+++ seeders/20230127083505-replies-seed.js
@@ -1,14 +1,28 @@
 'use strict'
 
 const faker = require('faker')
-const { REPLIES_PER_QUESTION } = require('../helpers/seeders-amount')
+const {
+  TEACHER_REPLIES_PER_QUESTION,
+  STUDENT_REPLIES_PER_QUESTION
+} = require('../helpers/seeders-amount')
 const { User, Question } = require('../models')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     const replies = []
-    const userIdArr = await User.findAll({
+    const teacherIdArr = await User.findAll({
+      raw: true,
+      nest: true,
+      where: {
+        role: 'teacher',
+        name: {
+          [Sequelize.Op.like]: 'seed-teacher%'
+        }
+      },
+      attributes: ['id']
+    })
+    const studentIdArr = await User.findAll({
       raw: true,
       nest: true,
       where: {
@@ -31,8 +45,19 @@ module.exports = {
     })
     questionIdArr.forEach((question, index) => {
       replies.push(
-        ...Array.from({ length: REPLIES_PER_QUESTION }, () => ({
-          UserId: userIdArr[Math.floor(Math.random() * userIdArr.length)].id,
+        ...Array.from({ length: TEACHER_REPLIES_PER_QUESTION }, () => ({
+          UserId:
+            teacherIdArr[Math.floor(Math.random() * teacherIdArr.length)].id,
+          QuestionId: question.id,
+          comment: 'seed-reply:' + faker.lorem.sentences(2),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }))
+      )
+      replies.push(
+        ...Array.from({ length: STUDENT_REPLIES_PER_QUESTION }, () => ({
+          UserId:
+            studentIdArr[Math.floor(Math.random() * studentIdArr.length)].id,
           QuestionId: question.id,
           comment: 'seed-reply:' + faker.lorem.sentences(2),
           createdAt: new Date(),
