96de81866abbf7fb3dfd5e9a20e6f443dbfc7720
HowardWu5566 <ggg1237890@gmail.com>
2023-03-05 14:38:35 +0800

    feat: validate questions and replies by express-validator

---
+++ middleware/validate.js
@@ -0,0 +1,60 @@
+const { body, validationResult } = require('express-validator')
+const { grade, subject } = require('../config/questionInfo')
+
+module.exports = {
+  // 問題驗證規則
+  questionValidator: [
+    body('title')
+      .trim()
+      .notEmpty()
+      .withMessage("can't be empty")
+      .isLength({ min: 3, max: 50 })
+      .withMessage('must be between 3 and 50 characters'),
+    body('description')
+      .trim()
+      .notEmpty()
+      .withMessage("can't be empty")
+      .isLength({ min: 10, max: 500 })
+      .withMessage('must be between 10 and 500 characters'),
+    body('isAnonymous').trim().isBoolean().withMessage('must be a Boolean'),
+    body('grade').isIn(grade),
+    body('subject').isIn(subject)
+  ],
+
+  // 驗證問題
+  questionValidate: (req, res, next) => {
+    const errorInfo = validationResult(req)
+    // 編輯錯誤訊息
+    const errorMsg = errorInfo.errors.reduce((obj, error) => {
+      obj[error.param] = error.msg
+      return obj
+    }, {})
+    if (!errorInfo.isEmpty()) {
+      return res.status(422).json({
+        status: 'error',
+        message: errorMsg
+      })
+    }
+    next()
+  },
+
+  // 回覆驗證規則
+  replyValidator: body('comment')
+    .trim()
+    .notEmpty()
+    .withMessage("can't be empty")
+    .isLength({ max: 500 })
+    .withMessage("can't exceed 500 characters"),
+
+  //驗證回覆
+  replyValidate: (req, res, next) => {
+    const errorInfo = validationResult(req)
+    if (!errorInfo.isEmpty()) {
+      return res.status(422).json({
+        status: 'error',
+        message: errorInfo.errors[0].msg
+      })
+    }
+    next()
+  }
+}
--- routes/modules/question.js
+++ routes/modules/question.js
@@ -1,14 +1,32 @@
 const router = require('express').Router()
 const questionController = require('../../controllers/question-controller')
 const upload = require('../../middleware/multer')
+const {
+  questionValidator,
+  questionValidate,
+  replyValidator,
+  replyValidate
+} = require('../../middleware/validate')
 
 router.get('/', questionController.getQuestions)
-router.post('/', upload.array('images', 5), questionController.postQuestion)
+router.post(
+  '/',
+  upload.array('images', 5),
+  questionValidator,
+  questionValidate,
+  questionController.postQuestion
+)
 router.get('/:id', questionController.getQuestion)
 router.put('/:id', questionController.putQuestion)
 router.delete('/:id', questionController.deleteQuestion)
 router.get('/:id/replies', questionController.getReplies)
-router.post('/:id/replies', upload.array('images', 5), questionController.postReply)
+router.post(
+  '/:id/replies',
+  upload.array('images', 5),
+  replyValidator,
+  replyValidate,
+  questionController.postReply
+)
 router.post('/:id/like', questionController.postQuestionLike)
 router.delete('/:id/like', questionController.deleteQuestionLike)
 
