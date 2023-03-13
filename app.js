fa3acbe2352734fdf3256f3ae3c3d398ab81f76f
HowardWu5566 <ggg1237890@gmail.com>
2023-03-13 10:34:48 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-
+const cors = require('cors')
 const routes = require('./routes')
 const { errorHandler } = require('./middleware/error-handler')
 const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
